fec4c680f92511937e28ba3bd8baa27b09f420b6
Sam Xie <sam@samxie.me>
2023-04-28 21:57:23 -0700

    Replace deprecated methods

--- instruments.go
+++ instruments.go
@@ -19,7 +19,6 @@ import (
 	"strings"
 
 	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/instrument"
 )
 
 const (
@@ -27,18 +26,18 @@ const (
 )
 
 type dbStatsInstruments struct {
-	connectionMaxOpen                instrument.Int64ObservableGauge
-	connectionOpen                   instrument.Int64ObservableGauge
-	connectionWaitTotal              instrument.Int64ObservableCounter
-	connectionWaitDurationTotal      instrument.Float64ObservableCounter
-	connectionClosedMaxIdleTotal     instrument.Int64ObservableCounter
-	connectionClosedMaxIdleTimeTotal instrument.Int64ObservableCounter
-	connectionClosedMaxLifetimeTotal instrument.Int64ObservableCounter
+	connectionMaxOpen                metric.Int64ObservableGauge
+	connectionOpen                   metric.Int64ObservableGauge
+	connectionWaitTotal              metric.Int64ObservableCounter
+	connectionWaitDurationTotal      metric.Float64ObservableCounter
+	connectionClosedMaxIdleTotal     metric.Int64ObservableCounter
+	connectionClosedMaxIdleTimeTotal metric.Int64ObservableCounter
+	connectionClosedMaxLifetimeTotal metric.Int64ObservableCounter
 }
 
 type instruments struct {
 	// The latency of calls in milliseconds
-	latency instrument.Float64Histogram
+	latency metric.Float64Histogram
 }
 
 func newInstruments(meter metric.Meter) (*instruments, error) {
@@ -47,8 +46,8 @@ func newInstruments(meter metric.Meter) (*instruments, error) {
 
 	if instruments.latency, err = meter.Float64Histogram(
 		strings.Join([]string{namespace, "latency"}, "."),
-		instrument.WithDescription("The latency of calls in milliseconds"),
-		instrument.WithUnit("ms"),
+		metric.WithDescription("The latency of calls in milliseconds"),
+		metric.WithUnit("ms"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create latency instrument, %v", err)
 	}
@@ -62,50 +61,50 @@ func newDBStatsInstruments(meter metric.Meter) (*dbStatsInstruments, error) {
 
 	if instruments.connectionMaxOpen, err = meter.Int64ObservableGauge(
 		strings.Join([]string{namespace, subsystem, "max_open"}, "."),
-		instrument.WithDescription("Maximum number of open connections to the database"),
+		metric.WithDescription("Maximum number of open connections to the database"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionMaxOpen instrument, %v", err)
 	}
 
 	if instruments.connectionOpen, err = meter.Int64ObservableGauge(
 		strings.Join([]string{namespace, subsystem, "open"}, "."),
-		instrument.WithDescription("The number of established connections both in use and idle"),
+		metric.WithDescription("The number of established connections both in use and idle"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionOpen instrument, %v", err)
 	}
 
 	if instruments.connectionWaitTotal, err = meter.Int64ObservableCounter(
 		strings.Join([]string{namespace, subsystem, "wait"}, "."),
-		instrument.WithDescription("The total number of connections waited for"),
+		metric.WithDescription("The total number of connections waited for"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionWaitTotal instrument, %v", err)
 	}
 
 	if instruments.connectionWaitDurationTotal, err = meter.Float64ObservableCounter(
 		strings.Join([]string{namespace, subsystem, "wait_duration"}, "."),
-		instrument.WithDescription("The total time blocked waiting for a new connection"),
-		instrument.WithUnit("ms"),
+		metric.WithDescription("The total time blocked waiting for a new connection"),
+		metric.WithUnit("ms"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionWaitDurationTotal instrument, %v", err)
 	}
 
 	if instruments.connectionClosedMaxIdleTotal, err = meter.Int64ObservableCounter(
 		strings.Join([]string{namespace, subsystem, "closed_max_idle"}, "."),
-		instrument.WithDescription("The total number of connections closed due to SetMaxIdleConns"),
+		metric.WithDescription("The total number of connections closed due to SetMaxIdleConns"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionClosedMaxIdleTotal instrument, %v", err)
 	}
 
 	if instruments.connectionClosedMaxIdleTimeTotal, err = meter.Int64ObservableCounter(
 		strings.Join([]string{namespace, subsystem, "closed_max_idle_time"}, "."),
-		instrument.WithDescription("The total number of connections closed due to SetConnMaxIdleTime"),
+		metric.WithDescription("The total number of connections closed due to SetConnMaxIdleTime"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionClosedMaxIdleTimeTotal instrument, %v", err)
 	}
 
 	if instruments.connectionClosedMaxLifetimeTotal, err = meter.Int64ObservableCounter(
 		strings.Join([]string{namespace, subsystem, "closed_max_lifetime"}, "."),
-		instrument.WithDescription("The total number of connections closed due to SetConnMaxLifetime"),
+		metric.WithDescription("The total number of connections closed due to SetConnMaxLifetime"),
 	); err != nil {
 		return nil, fmt.Errorf("failed to create connectionClosedMaxLifetimeTotal instrument, %v", err)
 	}
